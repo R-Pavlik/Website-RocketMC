bfc54d9559705b9afad2d0413253ed055c84e481
R-Pavlik <100788815+R-Pavlik@users.noreply.github.com>
2024-08-24 17:21:24 +0200

    LoginScreen.tsxmoved to /auth/page.tsx.

--- app/admin/auth/page.tsx
+++ app/admin/auth/page.tsx
@@ -1,19 +1,33 @@
 "use client"; // Ujistěte se, že komponenta je klientská
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../api/firebaseConfig'; // Upravte cestu podle struktury vašeho projektu
+import { useRouter } from 'next/navigation'; // Import useRouter
 
-export default function LoginScreen() {
+export default function Auth() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState<string | null>(null);
+    const router = useRouter();
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+    // Kontrola stavu přihlášení při načtení stránky
+    useEffect(() => {
+        const storedAuthStatus = localStorage.getItem('isAuthenticated');
+        if (storedAuthStatus === 'true') {
+            setIsAuthenticated(true);
+            router.push('/admin/overview'); // Přesměrování, pokud uživatel již je přihlášen
+        }
+    }, [router]);
 
     const handleLogin = async (e: React.FormEvent) => {
         e.preventDefault();
         try {
             await signInWithEmailAndPassword(auth, email, password);
-            // Přesměrování nebo jiné akce po úspěšném přihlášení mohou být přidány zde
+            setIsAuthenticated(true);
+            localStorage.setItem('isAuthenticated', 'true'); // Uložení stavu přihlášení do localStorage
+            router.push('/admin/overview'); // Přesměrování po úspěšném přihlášení
         } catch (error) {
             setError('Failed to login. Please check your credentials.');
         }
@@ -22,12 +36,10 @@ export default function LoginScreen() {
     return (
         <div className="flex flex-col items-center md:justify-center h-screen bg-darkgray">
             <div className='mb-4 mt-12 md:mt-1 flex justify-center items-center flex-col'>
-                <img src="rocketmc-logo.png" alt="" className=' w-[128px]'/>
-                <h1 className='   kanitfont text-4xl text-white'>LOGIN TO CONTINUE</h1>
-
+                <img src="rocketmc-logo.png" alt="" className='w-[128px]' />
+                <h1 className='kanitfont text-4xl text-white'>LOGIN TO CONTINUE</h1>
             </div>
             <div className="bg-darkergray p-8 rounded-lg shadow-lg md:w-[435px] w-full max-w-md flex justify-center items-center">
-                
                 <form onSubmit={handleLogin}>
                     <div className="mb-4">
                         <label className="block text-white font-bold mb-2 ml-2 lexendfont">EMAIL:</label>
