afdc7b379ec250d9707e8336a36712bd94d76322
R-Pavlik <100788815+R-Pavlik@users.noreply.github.com>
2024-08-24 16:46:00 +0200

    Added temp overview

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+"use client";
+import React from 'react';
+import { auth } from '../../api/firebaseConfig'
+import { signOut } from 'firebase/auth';
 
-export default function page() {
+export default function Page() {
+  const handleLogout = () => {
+    signOut(auth).then(() => {
+      // Uživateli se úspěšně podařilo se odhlásit.
+      console.log('Odhlášení proběhlo úspěšně');
+    }).catch((error) => {
+      // Nastala chyba při odhlašování.
+      console.error('Chyba při odhlašování: ', error);
+    });
+  };
+
   return (
-    <div>pssss</div>
-  )
+    <div className='w-screen h-screen bg-darkgray flex flex-col items-center justify-center'>
+      <h1 className='text-white mb-4'>Vítejte!</h1>
+      <button 
+        onClick={handleLogout} 
+        className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600'
+      >
+        Odhlásit se
+      </button>
+    </div>
+  );
 }
