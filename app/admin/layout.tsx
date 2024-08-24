4503bd93334afa99a6736ee87a166c7e95be6e69
R-Pavlik <100788815+R-Pavlik@users.noreply.github.com>
2024-08-24 17:21:52 +0200

    Some small changes.

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,14 +1,15 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-import { auth } from '../api/firebaseConfig'; // Upravte cestu podle struktury vašeho projektu
+import { auth } from '../api/firebaseConfig';
 import { onAuthStateChanged } from 'firebase/auth';
 import Navbar from './components/Navbar';
-import LoginScreen from './components/LoginScreen'; // Importujte komponentu přihlašovací stránky
-import Loader from '../utils/Loader';
+import { useRouter } from 'next/navigation';
+import AdminLoader from '../utils/AdminLoader';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
     const [loading, setLoading] = useState<boolean>(true);
+    const router = useRouter();
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -16,25 +17,21 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 setIsAuthenticated(true);
             } else {
                 setIsAuthenticated(false);
+                router.push("/admin/auth"); // Move redirection here
             }
             setLoading(false);
         });
 
         return () => unsubscribe();
-    }, []);
+    }, [router]);
 
     if (loading) {
-        return <Loader /> // Nebo nějaký indikátor načítání
+        return <AdminLoader />; // Loading indicator
     }
-
     return (
         <div>
             <Navbar />
-            {isAuthenticated ? (
-                <div>{children}</div> // Zobrazí přehledovou stránku nebo jiné děti
-            ) : (
-                <LoginScreen /> // Zobrazí přihlašovací stránku
-            )}
+            <div>{children}</div>
         </div>
     );
 }
