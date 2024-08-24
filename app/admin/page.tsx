481362a2be8e023a1d0b481c3e1cdc7633e3b20e
R-Pavlik <100788815+R-Pavlik@users.noreply.github.com>
2024-08-24 23:22:03 +0200

    Removed page.tsx from /admin (security reasons)..

--- app/admin/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-import React from 'react'
-import { useRouter } from 'next/navigation'
-
-export default function page() {
-  const router = useRouter()
-  return router.push("/admin/overview")
-}
