c2cb08d491920302877f282c647b1d5272418d6d
R-Pavlik <100788815+R-Pavlik@users.noreply.github.com>
2024-08-25 10:13:50 +0200

    Changed background color in admin overview.

--- app/admin/overview/components/PostInfo.tsx
+++ app/admin/overview/components/PostInfo.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function PostInfo() {
   return (
-    <div className='w-full h-full bg-black flex md:flex-row flex-col md:justify-center justify-start md:items-start  items-center gap-4'>
+    <div className='w-full h-full bg-darkestgray flex md:flex-row flex-col md:justify-center justify-start md:items-start  items-center gap-4'>
         <div className='md:w-[600px] w-[80%] h-[300px] bg-darkergray rounded-xl mt-6'></div>
         <div className='md:w-[600px] w-[80%] h-[300px] bg-darkergray rounded-xl mt-6'></div>
     </div>
